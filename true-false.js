e8b531fe45519921c8f7310333846e97da08221d
Masud3 <mdamdadulm79@gmail.com>
2021-02-02 07:21:56 -0800

    true vs false javascript

---
+++ true-false.js
@@ -0,0 +1,17 @@
+// Falsy value;
+// false
+// 0
+// ""
+// undefined
+// null
+// NaN
+
+// Truthy value
+// '0', ' ', [], {}
+let name ="false";
+if(name){
+    console.log("condition is true")
+}
+else{
+    console.log("condition is false")
+}
